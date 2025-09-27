218c3547972a1f61cc1443ead4c12c5954cdb150
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-09-27 16:53:55 -0300

    terminando o aside

--- bookmark-landing-page/src/components/AsideQuestions.jsx
+++ bookmark-landing-page/src/components/AsideQuestions.jsx
@@ -1,28 +1,31 @@
 import "../estilos/questions.css"
-import ArrowButton from '../images/icon-arrow.svg'
+import { useState } from "react"
 export default function AsideQuestions(){
     const lista = [
         {
             "title":"What is Bookmark?",
-            "arrowButton":ArrowButton,
             "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis."
         },
         {
             "title":"How can I request a new browser?",
-            "arrowButton":ArrowButton,
             "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis."
         },
         {
             "title":"Is there a mobile app?",
-            "arrowButton":ArrowButton,
             "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis."
         },
         {
             "title":"What about other Chromium browsers?",
-            "arrowButton":ArrowButton,
             "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis. quam ornare mattis"
         }
     ]
+
+    const [ativo,setAtivo]=useState(null)
+
+
+    const inverterArrowButton =(isOpen)=>{return isOpen ? <i class='fa-solid fa-chevron-up'></i> : <i class='fa-solid fa-chevron-down'></i>}
+    const handleToggle = (index)=>{setAtivo(ativo === index ? null : index)}
+
     return(
         <aside className="container-question">
             <div className="content">
@@ -33,20 +36,25 @@ export default function AsideQuestions(){
             </div>
             <div className="list-questions">
                 {
-                    lista.map(({title,arrowButton,description},index)=>(
-                        <div className="item" key={index}>
-                            <div className="item-topic">
-                                <h2>{title}</h2>
-                                <button className="btn_arrow">
-                                    <img src={arrowButton} onClick={()=>{
-                                    }}/>
-                                </button>
+                    lista.map(({title,description},index)=>{
+                        const isOpen = ativo === index
+                        return(
+                            <div className="item" key={index}>
+                                <div className="item-topic">
+                                    <h2>{title}</h2>
+                                    <button className="btn_arrow" onClick={()=>{handleToggle(index)}}>
+                                        {inverterArrowButton(isOpen)}
+                                    </button>
+                                </div>
+                                
+                               {isOpen && (
+                                <div className="container-description">
+                                    <p>{description}</p>
+                                </div>
+                                )}
                             </div>
-                            <div className="container-description">
-                                <p>{description}</p>
-                            </div>
-                        </div>
-                    ))
+                        )
+                    })
                 }
             </div>
         </aside>
