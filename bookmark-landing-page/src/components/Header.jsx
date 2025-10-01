8af18efc3dcb7b700b3a7de427bd261ca2f1f3a8
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-10-01 16:00:57 -0300

    adicionando mudanças no css na versão mobile

--- bookmark-landing-page/src/components/Header.jsx
+++ bookmark-landing-page/src/components/Header.jsx
@@ -1,18 +1,30 @@
 import LogoBookmarc from '../images/logo-bookmark.svg'
 import '../estilos/Header.css'
+import { useState } from 'react'
+
+import iconHamburger from '../images/icon-hamburger.svg'
+
+import iconSair from '../images/icon-close.svg'
 
 function Header(){
+    const [menuAberto, setMenuAberto] = useState(false);
     return(
         <header>
             <div className="header-img">
                 <img src={LogoBookmarc} alt="logo-bookmark" />
             </div>
-            <div className="header-buttons">
+            <button className='hamburger' onClick={()=>{
+                setMenuAberto(!menuAberto);
+            }}>
+                <img src={menuAberto ? iconSair:iconHamburger} alt="menu" />
+            </button>
+
+            <nav className={`header-buttons ${menuAberto ? "ativo":""}`}>
                 <a href="#Features">Features</a>
                 <a href="#Pricing">Pricing</a>
                 <a href="#Contact">Contact</a>
                 <a href="#" className='btn-login'>Login</a>
-            </div>
+            </nav>
         </header>
     )
 }
